6ac7db989aac2b3212b10658fb295c5df5136e8f
EnyMan <Mous.imp@gmail.com>
2024-07-23 00:11:21 +0200

    Improve loading of stored data

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from app_state import AppState, Workout
 import numpy as np
 from local_storage import StLocalStorage
 import json
+from functools import reduce
+from operator import add
 
 st_ls = StLocalStorage()
 
@@ -24,11 +26,14 @@ def save_data():
     )
 
 
-def load_data():
-    local_app_data = st_ls.get("app_data")
-    if local_app_data is None:
-        print("No data to load")
-        return
+def load_data(overwrite_text):
+    if overwrite_text is not None:
+        local_app_data = overwrite_text
+    else:
+        local_app_data = st_ls.get("app_data")
+        if local_app_data is None:
+            print("No data to load")
+            return
     print("Loading data")
     # st.write(local_app_data)
     st.session_state["recomended_weight_multiplier"] = local_app_data["recomended_weight_multiplier"]
@@ -97,7 +102,7 @@ def exercise_expander_content():
         )
         last_total_weight = last_sets * last_mean_weight * last_mean_reps
         previous_total_weight = previous_sets * previous_mean_weight * previous_mean_reps
-        st.metric("Last Total Weight", f"{last_total_weight:.1f} kg", delta=f"{last_total_weight - previous_total_weight:.1f} kg")
+        st.metric("Last Volume", f"{last_total_weight:.1f} kg", delta=f"{last_total_weight - previous_total_weight:.1f} kg")
     exercise_add_column, exercise_set_column, _ = st.columns([1, 1, 3])
     with exercise_add_column:
         if st.button("Add Workout", key=f"add_workout_{exercise_name}"):
@@ -125,10 +130,12 @@ def exercise_expander_content():
                 exercise.workouts[-1].add_set(weight, reps)
                 save_data()
 
-st_ls.get("app_data")
 
 if len(st.session_state["app_data"].exercises) == 0:
-    load_data()
+    local_data_overwrite = st_ls.get("app_data")
+    load_data_text = st.text(local_data_overwrite)
+    load_data(local_data_overwrite)
+    load_data_text.empty()
 
 st.sidebar.title("Gym Helper")
 
